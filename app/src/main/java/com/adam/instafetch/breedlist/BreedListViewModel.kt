b2abf58be7a83345edda976bdb501bd262a363c7
Adam <adam.aljidy@gmail.com>
2025-06-01 00:06:03 +0100

    Add dispatcher to BreedListViewModel to inject different one during unitTest. Formatting

--- app/src/main/java/com/adam/instafetch/breedlist/BreedListViewModel.kt
+++ app/src/main/java/com/adam/instafetch/breedlist/BreedListViewModel.kt
@@ -5,30 +5,32 @@ import androidx.lifecycle.viewModelScope
 import com.adam.instafetch.BaseViewModel
 import com.adam.instafetch.DogBreedModel
 import com.adam.instafetch.DogsRepo
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.IOException
 
-class BreedListViewModel(private val repo: DogsRepo) : BaseViewModel<DogBreedListState>(DogBreedListState()) {
+class BreedListViewModel(
+    private val repo: DogsRepo,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Main, // Injected in for testing
+) : BaseViewModel<DogBreedListState>(DogBreedListState()) {
     init {
         getBreeds()
     }
 
     private fun getBreeds() {
-        viewModelScope.launch {
+        viewModelScope.launch(dispatcher) {
             setState { copy(isLoading = true) }
             try {
-                val breeds =
-                    repo.getDogBreeds().sortedBy { it.breedId }
+                val breeds = repo.getDogBreeds().sortedBy { it.breedId }
                 setState { copy(breeds = breeds, isLoading = false) }
             } catch (e: IOException) {
                 Log.e(this::class.simpleName, "Couldn't get breeds", e)
-
                 setState { copy(isError = true, isLoading = false) }
             }
         }
     }
 }
-
 
 data class DogBreedListState(
     val breeds: List<DogBreedModel> = listOf(),
