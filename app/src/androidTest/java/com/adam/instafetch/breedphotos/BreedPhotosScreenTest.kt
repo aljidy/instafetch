3613a7fd5492fa0820d554b518cd093ccd7ae874
Adam <adam.aljidy@gmail.com>
2025-06-02 01:20:40 +0100

    Update BreedPhotosScreenKtTest.kt to BreedPhotosScreenTest.kt

--- app/src/androidTest/java/com/adam/instafetch/breedphotos/BreedPhotosScreenTest.kt
+++ app/src/androidTest/java/com/adam/instafetch/breedphotos/BreedPhotosScreenTest.kt
@@ -26,7 +26,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class BreedPhotosScreenKtTest {
+class BreedPhotosScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
